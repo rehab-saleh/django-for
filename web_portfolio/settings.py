b53bc6466da8d7b1f2b4361798dfefe5e033f888
Your Name <you@example.com>
2022-09-14 14:26:45 +0200

    d

--- web_portfolio/settings.py
+++ web_portfolio/settings.py
@@ -125,3 +125,5 @@ MEDIA_URL = '/images/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DISABLE_COLLECTSTATIC = 1
